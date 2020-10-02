b1a0ae1c2c19d061ea8a05d41323767692538113
Gokul <Gokul16.619@gmail.com>
2020-10-02 19:26:48 -0400

    Created some changes in authentication

--- gbc-Project/gbc-Project/ViewController.swift
+++ gbc-Project/gbc-Project/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var passWord: UITextField!
+    @IBOutlet weak var errorDisplay: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +21,37 @@ class ViewController: UIViewController {
     }
 
     @IBAction func loginButton(_ sender: UIButton) {
-        Displayname()
+        
+        if(userName.text == "" || passWord.text == "")
+        {
+            errorDisplay.text = "Please enter valid credentials"
+            return
+        }
+        else{
+            Auth.auth().signIn(withEmail: userName.text!.lowercased(), password: passWord.text!) {authResult, error in
+                if(authResult != nil)
+                {
+                    print("User Found")
+                    self.errorDisplay.text = "User Found"
+                    self.userName.text = ""
+                    self.passWord.text = ""
+                }
+                else{
+                    print("User Not Found")
+                    self.errorDisplay.text = "User Not Found"
+                    self.userName.text = ""
+                    self.passWord.text = ""
+                }
+            }
+            
+            
+            
+            
+        }
+        
+        
+        
+        
     }
     
     @IBAction func signUp(_ sender: UIButton) {
@@ -29,16 +60,9 @@ class ViewController: UIViewController {
     }
     
     
-    func Displayname(){
-        print("User Name : \(userName.text!) \nPassword : \(passWord.text!)")
-        Auth.auth().signIn(withEmail: userName.text!.lowercased(), password: passWord.text!) {authResult, error in
-//          guard let strongSelf = self else { return }
-            print(error)
-            print(authResult)
-        }
-        userName.text = ""
-        passWord.text = ""
-    }
+    
+        
+    
     
     
 }

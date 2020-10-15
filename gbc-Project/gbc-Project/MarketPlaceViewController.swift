76b8fa1ee5ed71be6d1221773e91ba8ccfab3c29
Gokul <Gokul16.619@gmail.com>
2020-10-15 14:39:43 -0400

    Added Market Place file and added Image Background

---
+++ gbc-Project/gbc-Project/MarketPlaceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MarketPlaceViewController.swift
+//  gbc-Project
+//
+//  Created by Gokul Murugan on 2020-10-15.
+//
+
+import UIKit
+
+class MarketPlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "marketplace.jpg")!)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

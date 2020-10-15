18af99c5e79b4f6091bcb7a87e4760d51b9eda63
Gokul <Gokul16.619@gmail.com>
2020-10-15 15:35:18 -0400

    Table view added, Need to implement fire base data to Table view

--- gbc-Project/gbc-Project/MarketPlaceViewController.swift
+++ gbc-Project/gbc-Project/MarketPlaceViewController.swift
@@ -13,29 +13,35 @@ class MarketPlaceViewController: UIViewController, UITableViewDelegate, UITableV
 
     @IBOutlet weak var keyWord: UITextField!
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "marketplace.jpg")!)
+        
         
     }
     
 
     @IBAction func keyWordSearchButton(_ sender: Any) {
         
+        print("Button tapped")
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+        cell?.textLabel?.text = "Hello"
+        return cell!
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        print("You have selected cell at index \(indexPath)")
     }
 
 }

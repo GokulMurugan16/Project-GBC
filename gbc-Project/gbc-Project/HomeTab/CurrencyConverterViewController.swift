513bb3989f8a91414f0933d0beb0f767387fda55
Devjot Sandhu <devjotsandhu@gmail.com>
2020-10-22 13:46:54 -0400

    Added all the functions related to Currency Converter and debugged the decimal places in labels

--- gbc-Project/gbc-Project/HomeTab/CurrencyConverterViewController.swift
+++ gbc-Project/gbc-Project/HomeTab/CurrencyConverterViewController.swift
@@ -9,8 +9,20 @@ import UIKit
 
 class CurrencyConverterViewController: UIViewController {
     
+    // MARK: Variables
+    
     private var results: CurrencyRates?
-
+    private var currencyDict : [String:Double] = [:]
+    private var activeCurrency: Double = 0.0
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var currencySegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var exchangeRateLabel: UILabel!
+    @IBOutlet weak var inputAmount: UITextField!
+    @IBOutlet weak var outputAmountLabel: UILabel!
+    
     // MARK: DEFAULT FUNCTIONS
     
     override func viewDidLoad() {
@@ -21,31 +33,80 @@ class CurrencyConverterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // MARK: Button Function
+    
+    
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        print("Convert Button Pressed")
+        
+        if (inputAmount.text == "")
+        {
+            let alert = UIAlertController(title: "Error", message: "Enter a valid amount.", preferredStyle: UIAlertController.Style.alert)
+
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+            self.present(alert, animated: true, completion: nil)
+        }
+        else
+        {
+            outputAmountLabel.text = String((Double(inputAmount.text!)! / activeCurrency).round(to: 2)) + " CAD"
+        }
+        
+    }
+    
+    // MARK: Segment Functions
+    
+    @IBAction func currencyChanged(_ sender: Any) {
+        
+        switch currencySegmentControl.selectedSegmentIndex
+        {
+        case 0:
+            print ("Indian Rupee selected");
+            exchangeRateLabel.text = "1 CAD = " + String(currencyDict["INR"]!.round(to: 2)) + " Indian Rupees"
+            activeCurrency = currencyDict["INR"]!.round(to: 2)
+            break
+            
+        case 1:
+            print("US Dollar selected")
+            exchangeRateLabel.text = "1 CAD = " + String(currencyDict["USD"]!.round(to: 2)) + " US Dollar"
+            activeCurrency = currencyDict["USD"]!.round(to: 2)
+            break
+
+        case 2:
+            print("British Pound selected")
+            exchangeRateLabel.text = "1 CAD = " + String(currencyDict["GBP"]!.round(to: 2)) + " British Pound"
+            activeCurrency = currencyDict["GBP"]!.round(to: 2)
+            break
+            
+        default:
+            print("None selected")
+            break
+        }
+    }
+    
     // MARK: Data Load Functions
     
     func loadData() {
+        
            guard let url = URL(string: "https://api.exchangeratesapi.io/latest?base=CAD&symbols=USD,GBP,INR") else {
                print("Invalid URL")
                return
            }
            let request = URLRequest(url: url)
            URLSession.shared.dataTask(with: request) { data, response, error in
-               // below is step 4
+               
                if let data = data {
                    if let decodedResponse = try? JSONDecoder().decode(CurrencyRates.self, from: data) {
-                       // we have good data – go back to the main thread
+                       
                        DispatchQueue.main.async {
-                           // update our UI
-                           self.results = decodedResponse
-                        print(self.results!)
+                           
+                        self.results = decodedResponse
+                        self.currencyDict = self.results!.rates
+                       
                        }
-
-                       // everything is good, so we can exit
                        return
                    }
                }
-
-               // if we're still here it means there was a problem
                print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
 
            }.resume()
@@ -53,13 +114,18 @@ class CurrencyConverterViewController: UIViewController {
        }
     
     
-    
     // MARK: Segue Functions
     @IBAction func unwindToRootViewController(segue: UIStoryboardSegue) {
         print("Unwind to Root View Controller")
     }
     
+}
 
-   
+// MARK: Extension
 
+extension Double {
+    func round(to places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
 }

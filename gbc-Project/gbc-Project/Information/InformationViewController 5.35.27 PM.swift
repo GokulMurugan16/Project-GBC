2dd4432c8f9060601053b6955d0b26f4b5d4fb26
Gokul Murugan <Gokul16.619@gmail.com>
2020-12-16 23:16:33 -0500

    Final App

--- gbc-Project/gbc-Project/Information/InformationViewController 5.35.27 PM.swift
+++ gbc-Project/gbc-Project/Information/InformationViewController 5.35.27 PM.swift
@@ -12,19 +12,19 @@ import Firebase
 class InformationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var webArray = ["For Checklist before travel","For information on Covid","For information on Cerb","Important information for students","Ontario Colleges"]
-    var tipArray = ["Never share your sin number","Always Quarantine when you arrive","Never take off your mask in public","Collect your work/study permit at the Airport","Submit your ArriveCan form before hand"]
-
-    var rowSelected = 0
-    var webLinks = ["https://moving2canada.com/essential-list/","https://www.canada.ca/en/public-health/services/diseases/coronavirus-disease-covid-19.html","https://www.canada.ca/en/services/benefits/ei/cerb-application.html","https://www.alleducationschools.com/about/important-info/","https://www.ontario.ca/page/study-ontario-international-students"]
+        var tipArray = ["Never share your sin number","Always Quarantine when you arrive","Never take off your mask in public","Collect your work/study permit at the Airport","Submit your ArriveCan form before hand"]
+        var rowSelected = 0
+        var infoImage: String?
+        var webLinks = ["https://moving2canada.com/essential-list/","https://www.canada.ca/en/public-health/services/diseases/coronavirus-disease-covid-19.html","https://www.canada.ca/en/services/benefits/ei/cerb-application.html","https://www.alleducationschools.com/about/important-info/","https://www.ontario.ca/page/study-ontario-international-students"]
+        var detailInfo = ""
+        var detailTitle = ""
+        var webURL: String = ""
+        var db = Firestore.firestore()
+        var infoArray:[Info] = [Info]()
     
     @IBOutlet weak var webTableView: UITableView!
     @IBOutlet weak var adminInfoTableView: UITableView!
-    var detailInfo = ""
     
-    var webURL: String = ""
-    var db = Firestore.firestore()
-    
-    var infoArray:[Info] = [Info]()
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: true)
@@ -60,40 +60,51 @@ class InformationViewController: UIViewController, UITableViewDelegate, UITableV
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if tableView == self.webTableView{
-        let cell:UITableViewCell = (webTableView.dequeueReusableCell(withIdentifier: "cell") as UITableViewCell?)!
-                cell.textLabel?.text = webArray[indexPath.row]
-            return cell
-        }
-        else{
-            let cell:InfoTableViewCell = tableView.dequeueReusableCell(withIdentifier: "customInfoCell") as! InfoTableViewCell
-            
-               cell.infoTextView.text = infoArray[indexPath.row].title
-              cell.titleTextField.text =  infoArray[indexPath.row].Info
-               detailInfo = infoArray[indexPath.row].title
-               UIImage.loadFrom(url: URL(string: infoArray[indexPath.row].UImage)!) { i in
-                  cell.infoImageView.image = i
-            }
-            return cell
-        }
+                let cell:UITableViewCell = (webTableView.dequeueReusableCell(withIdentifier: "cell") as UITableViewCell?)!
+                        cell.textLabel?.text = webArray[indexPath.row]
+                    return cell
+                }
+                
+                else{
+                      let cell:InfoTableViewCell = tableView.dequeueReusableCell(withIdentifier: "customInfoCell") as! InfoTableViewCell
+                    
+                       cell.infoTextView.text = infoArray[indexPath.row].title
+                       cell.titleTextField.text =  infoArray[indexPath.row].Info
+                       cell.infoTextView.isEditable = false
+                       UIImage.loadFrom(url: URL(string: infoArray[indexPath.row].UImage)!) { i in
+                          cell.infoImageView.image = i
+                        
+                    }
+                    
+                    return cell
+                }
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if tableView == self.webTableView{
 
-        rowSelected = indexPath.row
-        print(webArray[rowSelected])
-        let vc = SFSafariViewController(url: URL(string: "\(webLinks[rowSelected])")!)
-        present(vc,animated: true)
-    }
-        else{
-            print("hello \(detailInfo)")
-            self.performSegue(withIdentifier: "detailedInfoSegue", sender: self)
+                rowSelected = indexPath.row
+                print(webArray[rowSelected])
+                let vc = SFSafariViewController(url: URL(string: "\(webLinks[rowSelected])")!)
+                present(vc,animated: true)
+            }
+                else{
+                    print("hello \(detailInfo)")
+                    detailInfo = infoArray[indexPath.row].Info
+                    detailTitle = infoArray[indexPath.row].title
+                    infoImage = infoArray[indexPath.row].UImage
+                  //  print("image path :\(infoImage)")
+                    
 
-        }
+                    self.performSegue(withIdentifier: "detailedInfoSegue", sender: self)
+
+                }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let pNumber = segue.destination as! detailedInfoViewController
-        pNumber.info = self.detailInfo
+               pNumber.info = self.detailTitle
+               pNumber.title = self.detailInfo
+               pNumber.selectedImage = self.infoImage
     }
     func loadFireBaseData() {
         db.collection("infoUpload").getDocuments { (snapshot, error) in
--- gbc-Project/gbc-Project/Information/detailedInfoViewController.swift
+++ gbc-Project/gbc-Project/Information/detailedInfoViewController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class detailedInfoViewController: UIViewController {
+    
     var info:String = ""
+    var titleToDisplay:String = ""
+    var selectedImage:String?
+    
+    
+    @IBOutlet weak var titletodisp: UILabel!
+    @IBOutlet weak var imageToDisp: UIImageView!
     @IBOutlet weak var detailTextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         detailTextView.text = info
+        titletodisp.text = titleToDisplay
+        UIImage.loadFrom(url: URL(string: selectedImage!)!) { i in
+            self.imageToDisp.image = i
+        }
         // Do any additional setup after loading the view.
     }
     
